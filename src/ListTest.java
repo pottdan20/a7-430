1ee58f0fe68afd5f881053257aa353b9160a2a07
John Clements <clements@racket-lang.org>
2023-02-23 23:36:46 -0800

    setup

--- src/ListTest.java
+++ src/ListTest.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -6,9 +7,26 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ListTest {
 
-  @Test
-  public void testLength() {
-    List<Integer> myList = new ArrayList<>();
-    assertEquals(0, myList.size());
+  // ONLY USE THIS FUNCTION TO CONSTRUCT LISTS FOR TESTING...
+  public <E> List<E>makeList(E[] array){
+    return Arrays.asList(array);
   }
+  
+
+  // the length() method:
+
+  // Specification:
+
+  // This part should be a textual specification of
+  // how the length method should behave...
+
+  // Tests:
+
+  @Test
+  public void testLength1() {
+    Integer[] ar = {3,4,5};
+    List<Integer> l = makeList(ar);
+    assertEquals(0, l.size());
+  }
+
 }
